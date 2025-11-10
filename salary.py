ab2de0e083c4f52b5cd832c2300d1e3a63e3e2d1
SayedMohammedYahiyaKhadri <sayedyahiya18@gmail.com>
2025-11-10 16:25:40 +0530

    Update salary.py

--- salary.py
+++ salary.py
@@ -1,5 +1,14 @@
-salary = 
-bonus = 0.10 * salary
-total_salary = salary + bonus
+import sys
+if  len(sys.argv) ==2:
+  script_salary = sys.argv[0]
+  salary = sys.argv[1]
+  print("User provided inputs values:")
+else:
+   script_salary = sys.argv[0]
+   salary = "6000"
+
+bonus = 0.10*salary
+total_salary = bonus+ salary
+
 print("Bonus Amount: ₹", bonus)
 print("Total Salary: ₹", total_salary)
